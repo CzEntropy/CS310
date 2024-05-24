b9423138d6c272435606f0cf3ceca08dc82a8681
CzEntropy <cristiantaylor08@gmail.com>
2024-05-24 00:09:25 -0400

    Week 11 Discussion

--- Module 11/Taylor_Discussion 11.cpp
+++ Module 11/Taylor_Discussion 11.cpp
@@ -1,3 +1,35 @@
 /*Create a simple C++ program using templates and at least two errors. 
-*
 //The program should not execute and there should be at least two errors in the code.
+*/
+
+#include <iostream>
+
+// Template function to add two numbers
+template <typename T>
+T add(T a, T b) {
+    return a + b;
+}
+
+// Template class to hold a value
+template [typename T]
+class ValueHolder {
+public:
+    ValueHolder(T value) : value(value) {}
+    T getValue() const { return value; }
+private:
+    T value;
+};
+
+int main() {
+  
+ ValueHolder intHolder(10); 
+
+
+    std::string result = add<std::string>("Hello, ", "world!"); 
+
+
+    std::cout << "Integer Value: " << intHolder.getValue() << std::endl;
+    std::cout << "Addition Result: " << result << std::endl;
+
+    return 0;
+}
