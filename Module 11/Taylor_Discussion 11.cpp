2b4b05000534dd86c4c924ba4527a587750fbd7f
CzEntropy <cristiantaylor08@gmail.com>
2024-05-24 00:23:38 -0400

    typo commit

--- Module 11/Taylor_Discussion 11.cpp
+++ Module 11/Taylor_Discussion 11.cpp
@@ -1,4 +1,7 @@
-/*Create a simple C++ program using templates and at least two errors. 
+/*
+Cristian Taylor
+5/23/24
+Create a simple C++ program using templates and at least two errors. 
 //The program should not execute and there should be at least two errors in the code.
 */
 
